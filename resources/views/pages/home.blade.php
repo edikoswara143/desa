f9e0a837bce1dbc61b30f66d4e97532226cd4895
Edi Koswara <edikoswara143@gmail.com>
2025-03-26 20:58:57 +0700

    fiture home page

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,4 +1,13 @@
 @extends('layouts.main')
 @section('content')
-  i am the home page
+    <div class="py-20">
+        <div class="w-full flex h-40 rounded-2xl bg-sky-600 p-4">
+            <div class="flex w-6/12">
+                helo
+            </div>
+            <div class="flex w-6/12">
+                helo
+            </div>
+        </div>
+    </div>
 @stop
