50139c0ef342a06d86485cb0c7c73a7ba1a6f8c3
Edi Koswara <edikoswara143@gmail.com>
2025-03-27 16:55:43 +0700

    add baner

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,12 +1,18 @@
 @extends('layouts.main')
 @section('content')
     <div class="py-20">
-        <div class="w-full flex h-40 rounded-2xl bg-sky-600 p-4">
-            <div class="flex w-6/12">
-                helo
-            </div>
-            <div class="flex w-6/12">
-                helo
+        <div class="w-full flex flex-col h-32 rounded-2xl bg-sky-800 p-2">
+            <div class="w-full h-full p-2 flex">
+                <div class="flex flex-col items-start text-center justify-center">
+                    <h3 class="text-sm text-center font-semibold text-sky-50 leading-relaxed">
+                        Selamat Datang di Sistem Tata Kelola
+                        Penduduk
+                        Terpadu</h3>
+                    <h3 class="text-xs text-center items-center font-medium leading-relaxed mt-2 text-sky-50 w-full"> -
+                        Paguyuban
+                        Desa Mandiri -
+                    </h3>
+                </div>
             </div>
         </div>
     </div>
