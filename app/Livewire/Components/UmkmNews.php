ae80f15fa78e4abf06cef42fbee20fea7ddcbd71
sabilala <edikoswara143@gmail.com>
2025-04-11 20:26:14 +0700

    umkm components

---
+++ app/Livewire/Components/UmkmNews.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Components;
+
+use Livewire\Component;
+
+class UmkmNews extends Component
+{
+    public function render()
+    {
+        return view('livewire.components.umkm-news');
+    }
+}
